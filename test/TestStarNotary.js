0d181274a223a1d37d3c0b97aad58e73710e8e51
Felipe Gontijo <epilef.gontijo@gmail.com>
2020-09-18 08:25:15 -0300

    test: Check if star can be claimed

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -17,3 +17,11 @@ it('has correct name', async () => {
     let starName = await instance.starName.call(); // .call() tells ethereum we don't want to persist any changes
     assert.equal(starName, 'Awesome Udacity Star'); // check if starName was initialized correctly
 });
+
+//can a star be claimed?
+it ('can be claimed', async () => {
+    let instance = await StarNotary.deployed();
+    await instance.claimStar({from: owner});
+    let starOwner = await instance.starOwner.call();
+    assert.equal(starOwner, owner);
+})
